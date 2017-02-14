08e3bcbced3f9c145ce6fbf428aaa963a13cf5a2
Peyman <mjpeyman.arab@gmail.com>
2017-02-14 19:22:41 +0330

    some changes
    made username public

--- Hw5Paint/src/main/User.java
+++ Hw5Paint/src/main/User.java
@@ -3,7 +3,7 @@ package main;
 public class User {
 
 	private int id;
-	private String userName;
+	pubic String userName;
 	private String passWord;
 
 	public User(int id, String userName, String passWord) {
@@ -41,8 +41,6 @@ public class User {
 		return passWord;
 	}
 
-	public void setPassWord(String passWord) {
-		this.passWord = passWord;
-	}
+	
 
 }
